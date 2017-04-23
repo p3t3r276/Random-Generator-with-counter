506d9b778813f92311ea3e49e6a3cb3fbf15816c
P3t3r276 <p3t3r276@gmail.com>
2017-04-23 09:39:40 +0700

    initialize files

---
+++ RndomGenerator/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace RndomGenerator
+{
+	public partial class MainWindow : Window
+	{
+		int interval = 10;
+
+		Random random = new Random();
+		private readonly DispatcherTimer counter = new DispatcherTimer();
+
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			counter.Interval = TimeSpan.FromMilliseconds(interval);
+			counter.Tick += CounterTick;
+		}
+
+		private void CounterTick(object sender, EventArgs e)
+		{
+			lblView.Content = random.Next(0, 1501);
+		}
+
+		private void OnKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Return)
+			{
+				counter.Stop();
+			}
+
+			if (e.Key == Key.K)
+			{
+				counter.Start();
+			}
+		}
+	}
+}
